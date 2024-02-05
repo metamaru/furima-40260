e1c9cce3de7920413090227350285deacf2b3dec
metamaru <kmeg.328@gmail.com>
2024-02-05 16:41:00 +0900

    Active Storageの導入とバリデーションの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,4 +7,15 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
   end
+
+  def create
+    @item = Item.new(item_params)
+
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:image, :item_name, :item_detail, :category_id, :product_condition, :delivery_charge_id, :prefecture_id, :delivery_day_id, :item_price).merge(user_id: current_user.id)
+  end
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,15 @@
 class Item < ApplicationRecord
   belongs_to :user
+  has_one_attached :image
+
+  with_options presence: true do
+    validates :item_name
+    validates :item_detail
+    validates :category_id
+    validates :product_condition_id
+    validates :delivery_charge_id
+    validates :prefecture_id
+    validates :delivery_day_id
+    validates :item_price
+  end
 end
