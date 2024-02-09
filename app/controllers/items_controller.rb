afa6f983bca05c582a916f1a674081dedc5f01d6
metamaru <kmeg.328@gmail.com>
2024-02-09 19:16:52 +0900

    一覧表示機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, only: %i[new create]
 
   def index
+    @items = Item.includes(:user).order('created_at DESC')
   end
 
   def new
