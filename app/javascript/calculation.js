f7f8fde162123abc25f69fd8f235ac2eb3c748e2
metamaru <kmeg.328@gmail.com>
2024-02-20 13:22:46 +0900

    販売手数料と利益の修正

--- app/javascript/calculation.js
+++ app/javascript/calculation.js
@@ -1,23 +1,17 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const itemPriceInput = document.getElementById("item-price");
-  console.log(ここまで)
-  if (itemPriceInput) {
-    itemPriceInput.addEventListener('input', calculation);
-    calculation();
-  }
-});
-
-function calculation() {
+window.addEventListener('turbo:load',() => {
   const priceInput = document.getElementById("item-price");
-  if (!priceInput) return; // ID が存在しない場合は処理を終了
+  priceInput.addEventListener("input", () => {
+    const inputValue = priceInput.value;
 
   const price = parseFloat(priceInput.value);
   if (isNaN(price)) return; // 入力が数値でない場合は処理を終了
 
   // 販売手数料と利益を計算（小数点以下の桁を表示しない）
-  const addTaxDom = Math.floor(price * 0.1);
-  const profit = Math.floor(price - addTaxDom);
+  const addTaxDom = document.getElementById("add-tax-price")
+  addTaxDom.innerHTML = (Math.floor(inputValue * 0.1));
 
-  document.getElementById("add-tax-dom").innerText = addTaxDom;
-  document.getElementById("profit").innerText = profit;
-}
+  const profit = document.getElementById("profit")
+  const taxValue = inputValue * 0.1
+  profit.innerHTML = (Math.floor(inoutValue - taxValue));
+  });
+});
